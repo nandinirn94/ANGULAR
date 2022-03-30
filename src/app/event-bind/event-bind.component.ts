93225de46d70c4303e1c484c91ebaa0d6d5cd0cb
= <=>
2022-03-30 20:29:33 +0530

    Merge branch 'master' of https://github.com/nandinirn94/ANGULAR

--- src/app/event-bind/event-bind.component.ts
+++ src/app/event-bind/event-bind.component.ts
@@ -2,7 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event-bind',
-  templateUrl: './event-bind.component.html',
+  template: `
+  <h2>
+            Greeting Phase
+        </h2>
+        <br>Prints on Console. <br>   <button (click)="console_writer()">Greet her</button>
+        <br>
+        <br>Prints on prompt.    <br> <button (click)="alert_box()">Greet him</button>
+        <br>
+        <br>Prints which type of event. <br><button (click)="onClick($event)"> Click here</button>
+  `,
   styleUrls: ['./event-bind.component.css']
 })
 export class EventBindComponent implements OnInit {
